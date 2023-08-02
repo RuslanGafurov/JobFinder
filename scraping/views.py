81612935414db3a2a7d3f79455c91041c08fe34d
RuslanGafurov <g.r.9@mail.ru>
2023-08-02 17:11:04 +0300

    Added home_view and VacancyListView

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from scraping.forms import VacancyFindForm
+from scraping.models import Vacancy
+from scraping.services import get_filter
+
+__all__ = (
+    'home_view',
+    'VacancyListView',
+)
+
+
+def home_view(request):
+    context = {'form': VacancyFindForm()}
+    return render(request, 'home.html', context)
+
+
+class VacancyListView(ListView):
+    model = Vacancy
+    template_name = 'scraping/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(VacancyListView, self).get_context_data(**kwargs)
+        context['city'] = self.request.GET.get('city')
+        context['language'] = self.request.GET.get('language')
+        context['form'] = VacancyFindForm()
+        return context
+
+    def get_queryset(self):
+        _filter = get_filter(
+            self.request.GET.get('city'),
+            self.request.GET.get('language')
+        )
+        return Vacancy.objects.filter(**_filter)

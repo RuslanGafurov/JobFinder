e686c4cf4754552d747f3bc47f87bec9c21416b5
RuslanGafurov <g.r.9@mail.ru>
2023-08-02 12:58:10 +0300

    Added models City and Language

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from scraping.models import City, Language
+
+admin.site.register(City)
+admin.site.register(Language)
--- scraping/models.py
+++ scraping/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+from scraping.services import from_cyrillic_to_latin
+
+
+class City(models.Model):
+    class Meta:
+        verbose_name = 'город'
+        verbose_name_plural = 'города'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = from_cyrillic_to_latin(str(self.name))
+        super().save(*args, **kwargs)
+
+    name = models.CharField(
+        max_length=50,
+        verbose_name='город',
+        unique=True,
+    )
+    slug = models.CharField(
+        max_length=50,
+        verbose_name='city',
+        unique=True,
+        blank=True
+    )
+
+
+class Language(models.Model):
+    class Meta:
+        verbose_name = 'язык программирования'
+        verbose_name_plural = 'языки программирования'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = from_cyrillic_to_latin(str(self.name))
+        super().save(*args, **kwargs)
+
+    name = models.CharField(
+        max_length=50,
+        verbose_name='язык',
+        unique=True,
+    )
+    slug = models.CharField(
+        max_length=50,
+        verbose_name='language',
+        unique=True,
+        blank=True
+    )

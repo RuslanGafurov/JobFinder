55a82f3ebadf05db7ce2795f13f9ad06c72d1041
RuslanGafurov <g.r.9@mail.ru>
2023-08-03 10:33:32 +0300

    Expanded VacancyFindForm

--- scraping/forms.py
+++ scraping/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from scraping.models import Vacancy, City, Language
+from scraping.models import City, Language
 
 
 class VacancyFindForm(forms.Form):
@@ -8,9 +8,17 @@ class VacancyFindForm(forms.Form):
         queryset=City.objects.all(),
         to_field_name='slug',
         required=False,
+        label='Город',
+        widget=forms.Select(attrs={
+            'class': 'form-control'
+        })
     )
     language = forms.ModelChoiceField(
         queryset=Language.objects.all(),
         to_field_name='slug',
         required=False,
+        label='Специальность',
+        widget=forms.Select(attrs={
+            'class': 'form-control'
+        })
     )

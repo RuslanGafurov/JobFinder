eebc47ab8146ade4e8a4593385bae598cf59a93e
RuslanGafurov <g.r.9@mail.ru>
2023-08-03 18:51:58 +0300

    Transferred headers

--- scraping/services.py
+++ scraping/services.py
@@ -49,3 +49,25 @@ def get_filter(city, language) -> dict:
         if language:
             _filter['language__slug'] = language
     return _filter
+
+
+headers = [
+    {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
+        'Accept':
+            'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    },
+    {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (HTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
+        'Accept':
+            'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    },
+    {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0',
+        'Accept':
+            'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    }
+]

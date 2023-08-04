90173912f8bc19bb72e2cac7040d1452e914969a
RuslanGafurov <g.r.9@mail.ru>
2023-08-04 09:09:45 +0300

    Minor edits

--- users/admin.py
+++ users/admin.py
@@ -59,10 +59,11 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ["email", "is_admin"]
+    list_display = ["email", "is_admin", "city", "language", "send_email"]
     list_filter = ["is_admin"]
     fieldsets = [
         (None, {"fields": ["email", "password"]}),
+        ('', {"fields": ["email", "password"]}),
         ("Permissions", {"fields": ["is_admin"]}),
     ]
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin

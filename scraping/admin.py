d64383fa6faf4e97162dd19deb5bc76016edbe1f
RuslanGafurov <g.r.9@mail.ru>
2023-08-04 16:18:26 +0300

    isort

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from scraping.models import City, Language, Vacancy, Error, Url
+from scraping.models import City, Error, Language, Url, Vacancy
 
 admin.site.register(City)
 admin.site.register(Language)

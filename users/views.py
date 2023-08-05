be7be7b0ba2d927e31209e2d0ad2166d49837922
RuslanGafurov <g.r.9@mail.ru>
2023-08-05 13:47:09 +0300

    Added login and logout views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from users.forms import UserLoginForm
+
+
+def login_view(request):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        data = form.cleaned_data
+        email = data.get('email')
+        password = data.get('password')
+        user = authenticate(request, email=email, password=password)
+        login(request, user)
+        return redirect('home')
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')

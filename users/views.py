6cc06d11392c04a865f0d3c611f0ef4637532844
RuslanGafurov <g.r.9@mail.ru>
2023-08-05 16:09:03 +0300

    Added update_view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm
 
 
 def login_view(request):
@@ -25,7 +25,30 @@ def registration_view(request):
     form = UserRegistrationForm(request.POST or None)
     if form.is_valid():
         new_user = form.save(commit=False)
-        new_user.set_password(form.cleaned_data['password'])
+        new_user.set_password(form.cleaned_data['password2'])
         new_user.save()
-        return render(request, 'users/login.html', {'new_user': new_user})
+        return render(request, 'users/registration_done.html', {'new_user': new_user})
     return render(request, 'users/registration.html', {'form': form})
+
+
+def update_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            form = UserUpdateForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.send_email = data['send_email']
+                user.save()
+                return redirect('users:update')
+        else:
+            form = UserUpdateForm(initial={
+                'city': user.city,
+                'language': user.language,
+                'send_email': user.send_email,
+            })
+        return render(request, 'users/update.html', {'form': form})
+    else:
+        return redirect('users:login')

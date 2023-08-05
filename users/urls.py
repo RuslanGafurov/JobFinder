d2c1613249bacc6d4f606b4d9c4b3e668167fdfa
RuslanGafurov <g.r.9@mail.ru>
2023-08-05 16:08:54 +0300

    Added profile url

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from users.views import login_view, logout_view, registration_view
+from users.views import login_view, logout_view, registration_view, update_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('update/', update_view, name='update'),
     path('registration/', registration_view, name='registration'),
 ]

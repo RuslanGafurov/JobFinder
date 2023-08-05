d2cfb99188778119bd99261671d5f193a77f744a
RuslanGafurov <g.r.9@mail.ru>
2023-08-05 14:50:44 +0300

    Added registration_view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login_view(request):
@@ -19,3 +19,13 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+def registration_view(request):
+    form = UserRegistrationForm(request.POST or None)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password'])
+        new_user.save()
+        return render(request, 'users/login.html', {'new_user': new_user})
+    return render(request, 'users/registration.html', {'form': form})

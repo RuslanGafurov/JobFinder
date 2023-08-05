413e788a035eeb4f86851ccfcdb9f4578bf51175
RuslanGafurov <g.r.9@mail.ru>
2023-08-05 16:17:26 +0300

    Added delete url

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import login_view, logout_view, registration_view, update_view
+from users.views import login_view, logout_view, registration_view, update_view, delete_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('delete/', delete_view, name='delete'),
     path('profile/', update_view, name='profile'),
     path('registration/', registration_view, name='registration'),
 ]

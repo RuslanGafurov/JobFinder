3d0eef274dffcad5999e8434217546c7c13002ef
RuslanGafurov <g.r.9@mail.ru>
2023-08-11 14:28:29 +0300

    Added logic for saving data to the Suggestion model

--- users/views.py
+++ users/views.py
@@ -1,22 +1,23 @@
-import datetime as dt
-
 from django.contrib import messages
 from django.contrib.auth import authenticate, get_user_model, login, logout
 from django.shortcuts import redirect, render
 
-from scraping.models import Error
+from scraping.models import Suggestion
 from users.forms import UserLoginForm, UserRegistrationForm, UserUpdateForm, UserContactForm
 
 User = get_user_model()
 
 
 def login_view(request):
+    """Вход в аккаунт"""
     form = UserLoginForm(request.POST or None)
     if form.is_valid():
-        data = form.cleaned_data
-        email = data.get('email')
-        password = data.get('password')
-        user = authenticate(request, email=email, password=password)
+        cln_data = form.cleaned_data
+        user = authenticate(
+            request,
+            email=cln_data.get('email'),
+            password=cln_data.get('password')
+        )
         login(request, user)
         messages.success(request, 'Вход в аккаунт выполнен')
         return redirect('home')
@@ -24,12 +25,14 @@ def login_view(request):
 
 
 def logout_view(request):
+    """Выход из аккаунта"""
     logout(request)
     messages.success(request, 'Выход из аккаунта выполнен')
     return redirect('home')
 
 
 def registration_view(request):
+    """Регистрация пользователя"""
     form = UserRegistrationForm(request.POST or None)
     if form.is_valid():
         new_user = form.save(commit=False)
@@ -41,16 +44,17 @@ def registration_view(request):
 
 
 def update_view(request):
+    """Обновление данных аккаунта"""
     contact_form = UserContactForm()
     if request.user.is_authenticated:
         user = request.user
         if request.method == 'POST':
             form = UserUpdateForm(request.POST)
             if form.is_valid():
-                data = form.cleaned_data
-                user.city = data['city']
-                user.language = data['language']
-                user.send_email = data['send_email']
+                cln_data = form.cleaned_data
+                user.city = cln_data['city']
+                user.language = cln_data['language']
+                user.send_email = cln_data['send_email']
                 user.save()
                 messages.success(request, 'Данные изменены')
                 return redirect('users:profile')
@@ -62,11 +66,11 @@ def update_view(request):
             })
         forms = {'form': form, 'contact_form': contact_form}
         return render(request, 'users/profile.html', forms)
-    else:
-        return redirect('users:login')
+    return redirect('users:login')
 
 
 def delete_view(request):
+    """Удаление аккаунта"""
     if request.user.is_authenticated:
         user = request.user
         if request.method == 'POST':
@@ -77,35 +81,17 @@ def delete_view(request):
 
 
 def contact_view(request):
+    """Отправка предложения от пользователя"""
     if request.method == 'POST':
         contact_form = UserContactForm(request.POST or None)
         if contact_form.is_valid():
             cln_data = contact_form.cleaned_data
-            city = cln_data.get('city')
-            language = cln_data.get('language')
-            email = cln_data.get('email')
-            errors_today = Error.objects.filter(timestamp=dt.date.today())
-            if errors_today.exists():
-                err = errors_today.first()
-                data = err.data.get('user_data', [])
-                data.append({
-                    'city': city,
-                    'language': language,
-                    'email': email,
-                })
-                err.data['user_data'] = data
-                err.save()
-            else:  # Нет ошибок за сегодняшний день
-                user_data = [{
-                    'city': city,
-                    'language': language,
-                    'email': email,
-                }]
-                Error(data=f'Предложения пользователей: {user_data}').save()
-
+            user_suggestion = Suggestion(
+                city=cln_data.get('city'),
+                language=cln_data.get('language'),
+                email=cln_data.get('email'),
+            )
+            user_suggestion.save()
             messages.success(request, 'Данные отправлены')
-            return redirect('users:profile')
-        else:  # Форма не валидна
-            return redirect('users:profile')
-    else:  # Метод GET
-        return redirect('users:login')
+        return redirect('users:profile')
+    return redirect('users:login')

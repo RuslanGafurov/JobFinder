1ca446b05ae01254fa4bbfaf297833dd99118c8b
RuslanGafurov <g.r.9@mail.ru>
2023-08-11 14:29:45 +0300

    Registered Suggestion model

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from scraping.models import City, Error, Language, Url, Vacancy
+from scraping.models import City, Error, Language, Url, Vacancy, Suggestion
 
 admin.site.register(City)
 admin.site.register(Language)
 admin.site.register(Vacancy)
 admin.site.register(Error)
 admin.site.register(Url)
+admin.site.register(Suggestion)

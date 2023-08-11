66d9fd84f1c462d113749940181ac88dae7303ef
RuslanGafurov <g.r.9@mail.ru>
2023-08-11 11:04:28 +0300

    Added contact url

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from users.views import (delete_view, login_view, logout_view,
-                         registration_view, update_view)
+                         registration_view, update_view, contact_view)
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('delete/', delete_view, name='delete'),
     path('profile/', update_view, name='profile'),
+    path('contact/', contact_view, name='contact'),
     path('registration/', registration_view, name='registration'),
 ]

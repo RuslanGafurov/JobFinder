44b46a6cbfe2a1186bc0a2a6acb5ede2e58e3561
RuslanGafurov <g.r.9@mail.ru>
2023-08-16 20:15:32 +0300

    Made changes to the logic for collecting vacancies

--- run_parsers.py
+++ run_parsers.py
@@ -1,8 +1,8 @@
-# _______________ Запуск Django ________________
-from scraping.services import launch_django  # |
-                                             # |
-launch_django()                              # |
-# _____________________________________________|
+# _____________ Запуск Django ______________
+from scraping.services import launch_django
+
+launch_django()
+# __________________________________________
 
 import asyncio
 import datetime as dt
@@ -12,38 +12,29 @@ from django.contrib.auth import get_user_model
 from django.db import DatabaseError
 
 from scraping.models import City, Error, Language, Url, Vacancy
-from scraping.parsers import headhunter
+from scraping.parsers import headhunter, super_job
 
 User = get_user_model()
 
 parsers = (
     (headhunter, 'headhunter'),
+    (super_job, 'super_job'),
 )
 jobs, errors = [], []
 today = dt.date.today()
 
 
-def get_settings() -> set[tuple[int, int]]:
-    """ Функция получения IDs города и языка """
-
-    users_qs = User.objects.filter(send_email=True).values()
-    ids = set((u['city_id'], u['language_id']) for u in users_qs)
-    return ids
-
-
-def get_urls(_settings: set[tuple[int, int]]) -> list[dict[str, Any]]:
+def get_urls() -> list[dict[str, Any]]:
     """ Функция получения адресов пользователей для поиска вакансий """
 
     urls_qs = Url.objects.all().values()
-    url_dct = {(u['city_id'], u['language_id']): u['urls'] for u in urls_qs}
     urls = []
-    for pair in _settings:
-        if pair in url_dct:
-            urls.append({
-                'city': pair[0],
-                'language': pair[1],
-                'urls': url_dct[pair],
-            })
+    for user in urls_qs:
+        urls.append({
+            'city': user['city_id'],
+            'language': user['language_id'],
+            'urls': user['urls'],
+        })
     return urls
 
 
@@ -65,28 +56,26 @@ def save_errors() -> None:
         )
 
 
-settings = get_settings()
-url_lst = get_urls(settings)
+url_lst = get_urls()
 
 
-# __________________________ Асинхронный запуск функций __________________________
-async def main(value: tuple[Any, str, int, int]) -> None:                       # |
-    func, url, city, language = value                                           # |
-    _job, _error = await loop.run_in_executor(None, func, url, city, language)  # |
-    errors.append(_error)                                                       # |
-    jobs.extend(_job)                                                           # |
-                                                                                # |
-                                                                                # |
-loop = asyncio.get_event_loop()                                                 # |
-tmp_tasks = [(func, data['urls'][key], data['city'], data['language'])          # |
-             for data in url_lst                                                # |
-             for func, key in parsers]                                          # |
-                                                                                # |
-tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])            # |
-                                                                                # |
-loop.run_until_complete(tasks)                                                  # |
-loop.close()                                                                    # |
-# ________________________________________________________________________________|
+# ________________________ Асинхронный запуск функций ________________________
+async def main(value: tuple[Any, str, int, int]) -> None:
+    func, url, city, language = value
+    _job, _error = await loop.run_in_executor(None, func, url, city, language)
+    errors.append(_error)
+    jobs.extend(_job)
+
+
+loop = asyncio.get_event_loop()
+tmp_tasks = [(func, data['urls'][key], data['city'], data['language'])
+             for data in url_lst
+             for func, key in parsers]
+
+tasks = asyncio.wait([loop.create_task(main(f)) for f in tmp_tasks])
+loop.run_until_complete(tasks)
+loop.close()
+# ____________________________________________________________________________
 
 # Сохранение полученных вакансий
 for job in jobs:
